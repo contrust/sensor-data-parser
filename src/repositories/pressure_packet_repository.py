a2f7567012a80985ab21faf2109f365969af0b14
Artyom Borisov <2001raf@gmail.com>
2023-11-25 23:14:28 +0500

    Added a count method for pressure_packet_repository.py.

--- src/repositories/pressure_packet_repository.py
+++ src/repositories/pressure_packet_repository.py
@@ -13,6 +13,9 @@ class PressurePacketRepository(SessionRepository):
     def get(self, current_value_counter: int) -> PressurePacket:
         return self.session.get(PressurePacket, current_value_counter)
 
+    def count(self):
+        return self.session.query(PressurePacket).count()
+
     def save_all(self, packets: Iterable[PressurePacket]) -> None:
         self.session.add_all(packets)
         self.session.commit()
