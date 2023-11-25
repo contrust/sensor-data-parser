6664363c5ed39a120e1bf2b819974dfbbbe3703b
Artyom Borisov <2001raf@gmail.com>
2023-11-25 23:13:47 +0500

    Renamed a function in parse_pressure_packets.py.

--- src/parsing/parse_pressure_packets.py
+++ src/parsing/parse_pressure_packets.py
@@ -1,9 +1,11 @@
 import logging
 
+from src.converters.hex_string_to_pressure_packet import (
+    hex_string_to_pressure_packet,
+)
 from src.expections.parsing_exception import ParsingException
 from src.expections.validation_exception import ValidationException
 from src.models.pressure_packet import PressurePacket
-from src.parsing.hex_string_to_packet import hex_string_to_packet
 from src.utils.list.chunks import chunks
 
 
@@ -11,7 +13,7 @@ def parse_pressure_packets(data: str) -> list[PressurePacket]:
     packets = []
     for chunk in chunks(data, 8):
         try:
-            packet = hex_string_to_packet(chunk)
+            packet = hex_string_to_pressure_packet(chunk)
             packets.append(packet)
         except (ParsingException, ValidationException) as e:
             logging.error(e)
