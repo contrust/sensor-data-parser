635ee094ac6a0638de46ae518e973e7e69fcfaf9
Artyom Borisov <2001raf@gmail.com>
2023-11-26 01:19:14 +0500

    Added the debug option processing in main.py.

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ from src.argparser.parser import get_parser
 from src.constants import LOGGING_FORMAT
 from src.models.pressure_packet import PressurePacket
 from src.parsing.parse_pressure_packets import parse_pressure_packets
-from src.repositories.pressure_packet_repository import PressurePacketRepository
+from src.repositories.pressure_packet_repository import (
+    PressurePacketRepository,
+)
 
 
 def main():
-    logging.basicConfig(format=LOGGING_FORMAT)
     parser = get_parser()
     args = parser.parse_args()
+    logging.basicConfig(
+        format=LOGGING_FORMAT,
+        level=logging.DEBUG if args.debug else logging.INFO,
+    )
     engine = create_engine(f"sqlite:///{args.db_path}", echo=True)
     if args.create_db:
         PressurePacket.metadata.create_all(bind=engine)
@@ -24,5 +29,5 @@ def main():
         pressure_packet_repository.save_all(packets)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
