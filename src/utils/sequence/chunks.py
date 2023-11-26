1e5ed6f919fd336408ac478e2c60d20f7de3f89f
Artyom Borisov <2001raf@gmail.com>
2023-11-26 07:01:27 +0500

    Reformatted chunks.py.

--- src/utils/sequence/chunks.py
+++ src/utils/sequence/chunks.py
@@ -3,4 +3,4 @@ from collections.abc import Sequence
 
 def chunks(sequence: Sequence, chunk_size: int):
     for i in range(0, len(sequence), chunk_size):
-        yield sequence[i: i + chunk_size]
+        yield sequence[i : i + chunk_size]
