8fbd6d3e6f7dcf33b0a915e67fbd8997855ca960
Artyom Borisov <2001raf@gmail.com>
2023-11-27 17:30:32 +0500

    Moved some fixtures to integration/conftest.py.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,6 +3,9 @@ import os
 import pytest
 from sqlalchemy import Engine, create_engine
 
+from sensor_data_parser.internal.models import PressurePacket
+from sensor_data_parser.internal.repositories import PressurePacketRepository
+
 
 @pytest.fixture(scope="session")
 def test_db_path() -> str:
@@ -13,3 +16,11 @@ def test_db_path() -> str:
 def engine(test_db_path: str) -> Engine:
     yield create_engine(f"sqlite:///{test_db_path}")
     os.remove(test_db_path)
+
+
+@pytest.fixture()
+def pressure_packet_repository(engine: Engine):
+    repository = PressurePacketRepository(engine)
+    PressurePacket.metadata.create_all(engine)
+    yield repository
+    PressurePacket.metadata.drop_all(engine)
