5408a2981e1fa27eb3c4345f57b72682885f1553
Artyom Borisov <2001raf@gmail.com>
2023-11-27 16:54:44 +0500

    Updated the test_save_all_adds_the_same_models test.

--- tests/integration/repositories/test_pressure_packet_repository.py
+++ tests/integration/repositories/test_pressure_packet_repository.py
@@ -22,4 +22,7 @@ def test_save_all_adds_the_same_models(
     assert db_pressure_packet is None
     pressure_packet_repository.save_all([pressure_packet])
     db_pressure_packet = pressure_packet_repository.get(1)
-    assert db_pressure_packet == pressure_packet
+    assert (db_pressure_packet.current_value_counter ==
+            pressure_packet.current_value_counter)
+    assert db_pressure_packet.pressure_value == pressure_packet.pressure_value
+    assert db_pressure_packet.status == pressure_packet.status
